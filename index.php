808bc101d6793fdabf0a755e547c2eadbd11cd93
Curtis Kennington <curtis@curtiskennington.co.uk>
2014-01-31 21:52:17 +0000

    Add FB Meta Tag

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    <meta name="description" content="Is Neil at Dobbies today? Find out here!">
 	    <meta name="keywords" content="Neil,Dobbies,Bolton,SSC,Location">
+	    <meta property="fb:app_id" content="721161077918183" />
 
 	    <!-- Styles -->
 	    <link href='http://fonts.googleapis.com/css?family=Raleway:800' rel='stylesheet' type='text/css'>
